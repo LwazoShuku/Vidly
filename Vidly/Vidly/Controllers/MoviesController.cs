846f06a6d044b235db1532d53194eefdd8cec26f
Lwazi Shangase <lwazoshuku@gmail.com>
2019-07-11 23:05:43 +0200

    Restrict Movie Management Operations to the Store Manager

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -27,9 +27,16 @@ namespace Vidly.Controllers
 
         public ViewResult Index()
         {
-            return View();
+            if (User.IsInRole(RoleName.CanManageMovies))
+            {
+                return View("List");
+            }
+
+            return View("ReadOnlyList");
+
         }
 
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public ViewResult New()
         {
             var genres = _context.Genres.ToList();
